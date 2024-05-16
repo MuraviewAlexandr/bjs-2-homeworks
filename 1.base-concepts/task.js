8137c026cf93cf04932242ab40198301b22a9eb5
Muraview Alexandr <muraview.alexandr@yandex.ru>
2024-05-16 13:24:10 +0300

    TaskOneTaskTwoUpdate

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,6 +1,6 @@
 "use strict"
 function solveEquation(a, b, c) {
-  let arr = []; {
+  let arr = []; 
     let d = b ** 2 - 4 * a * c;
   if (d > 0) {
     arr.push((-b + Math.sqrt(d)) / (2 * a));
@@ -8,16 +8,13 @@ function solveEquation(a, b, c) {
   } else if (d === 0) {
     arr.push(-b / (2 * a));
   }
-  }
-  
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  let percent = percent / 100;
-  let monthlyPercent = percent / 12;
-  let monthlyPayment = amount - contribution;
-  monthlyPayment = monthlyPayment / countMonths;
-  let totalAmount = monthlyPayment * countMonths;
+  let percentMonth = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let payment = creditBody * (percentMonth + (percentMonth / (((1 + percentMonth) ** countMonths) - 1)));
+  let totalAmount = (payment * countMonths).toFixed(2);
   return totalAmount;
 }
