aa195046a0e4d59c5c49e3a22636358a245721ec
Muraview Alexandr <muraview.alexandr@yandex.ru>
2024-05-24 17:25:08 +0300

    functionTaskAdd

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -51,7 +51,7 @@ function averageEvenElementsWorker(...arr) {
 function makeWork (arrOfArr, func) {
   let maxWorkerResult = -Infinity;
   for (let i = 0; i < arrOfArr.length; i++) {
-    const result = func(...arrOfArr[i]) 
+    const result = func(...arrOfArr[i]);
       if (result > maxWorkerResult) {
        maxWorkerResult = result;
       }
